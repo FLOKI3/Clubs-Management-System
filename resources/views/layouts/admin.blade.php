304150bf949895737712e2570137bd99c127ddca
Samy Akli <samyakli685@gmail.com>
2024-10-28 09:05:39 +0100

    Laravel Permissions & Roles

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>VALHALLA</title>
 
     <link rel="icon" href="{{ asset('assets/images/v.png') }}" type="image/png">
 
