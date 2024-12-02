81fe496f848fb34895712ede261db3cea9d3094c
Samy Akli <samyakli685@gmail.com>
2024-12-02 07:49:02 +0100

    Updates

--- resources/views/admin/users/role.blade.php
+++ resources/views/admin/users/role.blade.php
@@ -1,14 +1,16 @@
 <x-admin-layout>
     @section('title', 'USERS')
-    <div class="p-2 md:p-4">
-        <div class="w-full px-6 pb-8 sm:max-w-xl sm:rounded-lg">
-            <h2 class="text-2xl font-bold sm:text-xl">Profile Information</h2>
-            <div class="grid max-w-2xl mx-auto mt-8">
-                <form method="POST" action="{{ route('admin.users.update', $user->id) }}" enctype="multipart/form-data">
-                    @csrf
-                    @method('PUT')
-                    @include('admin.users.fields')
-                </form>
+    <div class="flex flex-col items-center">
+        <div style="padding: 20px" class="bg-white shadow-md rounded-lg">
+            <div class="w-full px-6 pb-8 sm:max-w-xl sm:rounded-lg">
+                <h2 class="text-2xl font-bold sm:text-xl">Profile Information</h2>
+                <div class="grid max-w-2xl mx-auto mt-8">
+                    <form method="POST" action="{{ route('admin.users.update', $user->id) }}" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        @include('admin.users.fields')
+                    </form>
+                </div>
             </div>
         </div>
     </div>
